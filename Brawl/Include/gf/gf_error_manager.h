0e79b142064662d804da6b03c400ee202dbbe8f1
RomThpt <reeflk8@gmail.com>
2026-08-23 17:49:10 +0200

    Type gfErrorManager size, instance and constructor

--- Brawl/Include/gf/gf_error_manager.h
+++ Brawl/Include/gf/gf_error_manager.h
@@ -10,9 +10,13 @@ public:
     bool unk0_b5 : 1;
     bool unk0_b4 : 1;
     bool unk0_b3 : 1;
+    char _1[0x1b];
 
+    gfErrorManager();
     void notifyError(u32 p1, u32 p2);
     void notifyErrorRecovery(bool p1);
     static gfErrorManager* getInstance();
+
+    static gfErrorManager* s_instance;
 };
-// TODO size assertion
+static_assert(sizeof(gfErrorManager) == 0x1c, "Class is wrong size!");
