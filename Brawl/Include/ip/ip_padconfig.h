2ab166d3d513244feb90bae274472f0990049e7c
RomThpt <reeflk8@gmail.com>
2026-08-23 17:57:12 +0200

    Type ipPadConfig size, instance, constructor and init

--- Brawl/Include/ip/ip_padconfig.h
+++ Brawl/Include/ip/ip_padconfig.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include <StaticAssert.h>
 #include <types.h>
 
 struct ipPadConfig {
+    char _0[0xB4];
+    u8 m_xB4;
+    char _B5[0xF7];
+
+    ipPadConfig() {
+        m_xB4 = 0;
+        init();
+    }
+    void init();
     static ipPadConfig* getInstance();
     s32 getPlayerNo(s32 p1);
+
+    static ipPadConfig* s_instance;
 };
-// TODO size
+static_assert(sizeof(ipPadConfig) == 0x1ac, "Class is wrong size!");
